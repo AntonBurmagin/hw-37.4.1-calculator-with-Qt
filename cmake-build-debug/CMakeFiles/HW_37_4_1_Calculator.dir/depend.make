70f19e37e669d4d90cea3fcb8f4b276a0b66b011
Anton <burmagin.ant@mail.ru>
2022-05-14 17:03:48 +0300

    ignore cmake-build-debug

--- cmake-build-debug/CMakeFiles/HW_37_4_1_Calculator.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for HW_37_4_1_Calculator.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/CMakeFiles/HW_37_4_1_Calculator_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/HW_37_4_1_Calculator_autogen"
-  "HW_37_4_1_Calculator_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/HW_37_4_1_Calculator_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
